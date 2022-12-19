50d50e58610e228fb2abd1e6f5bf907f891d76b1
unknown <pritishetye209@gmail.com>
2022-12-19 12:19:30 +0530

    demo1 test cases

---
+++ src/main/java/PageLayer/Demo1Page.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class Demo1Page {
+
+	public void demo1()
+	{
+		System.out.println("demo");
+	}
+
+}
---
+++ src/test/java/TestLayer/Demo1Test.java
@@ -0,0 +1,14 @@
+package TestLayer;
+
+import PageLayer.Demo1Page;
+
+public class Demo1Test {
+
+	public static void main(String[] args)
+	{
+		Demo1Page d=new Demo1Page();
+	    d.demo1();
+
+	}
+
+}
